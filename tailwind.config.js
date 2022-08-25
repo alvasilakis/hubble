fae28f1b7c1b94a6992df0fe3da2cf14699e1ce0
alexander.vasilakis <alexander.vasilakis@angrycreative.uk>
2022-08-25 18:07:11 +0300

    Spacer Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-	theme: {
+  theme: {
     colors: {
       'main-bg-color'    : '#0A1628',
       'main-purple'      : '#8655FF',
@@ -18,21 +18,21 @@ module.exports = {
       'blue'             : '#3AFCFC',
       'pink'             : '#E359FF',
     },
-		fontFamily: {
-			'roobert': ['"Roobert"'],
+    fontFamily: {
+      'roobert': ['"Roobert"'],
     },
-		content: {
-			'arrow'        : 'url("/icons/arrow.svg")',
-			'btn-arrow'    : 'url("/icons/purple-btn-arrow.svg")',
-			'alt-btn-arrow': 'url("/icons/black-btn-arrow.svg")',
-		},
-		backgroundImage: {
-			'role-gradient' : 'radial-gradient(50% 50% at 50% 50%, #E359FF 0%, #8655FF 100%)',
-			'black-gradient': 'linear-gradient(180deg, rgba(10, 22, 40, 0) 0%, #0A1628 90%)',
-		},
-		maxWidth: {
-			'wide-container': '1360px',
-			'container'     : '1200px',
+    content: {
+      'arrow'        : 'url("/icons/arrow.svg")',
+      'btn-arrow'    : 'url("/icons/purple-btn-arrow.svg")',
+      'alt-btn-arrow': 'url("/icons/black-btn-arrow.svg")',
+    },
+    backgroundImage: {
+      'role-gradient' : 'radial-gradient(50% 50% at 50% 50%, #E359FF 0%, #8655FF 100%)',
+      'black-gradient': 'linear-gradient(180deg, rgba(10, 22, 40, 0) 0%, #0A1628 90%)',
+    },
+    maxWidth: {
+      'wide-container': '1360px',
+      'container'     : '1200px',
     },
   },
   plugins: [],
